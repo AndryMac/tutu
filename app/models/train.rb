cb1086b21dd31ed7bd8b6cdaca3967ca389fe0f5
AndryMac <promasalkin@yandex.ru>
2017-02-14 13:59:44 +0500

    Добавил в модель связь

--- app/models/train.rb
+++ app/models/train.rb
@@ -3,4 +3,5 @@ class Train < ApplicationRecord
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   belongs_to :route
   has_many :tickets
+  has_many :carriages
 end
