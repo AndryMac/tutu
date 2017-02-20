b19acb83c1745bc39d73f4510f9f9015d2a000da
AndryMac <promasalkin@yandex.ru>
2017-02-20 12:01:13 +0500

    Добавил скоупы

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -2,10 +2,16 @@ class Carriage < ApplicationRecord
   validates :type_wagon, :number_top_place, :number_bottom_place,  presence: true
   belongs_to :train
 
-  TYPES = %w(EconomCar CompartmentCar)
+  TYPES = {
+      EconomCar: 'Плацкарт',
+      CompartmentCar: 'Купе'
+  }
 
   def self.types
     TYPES
   end
+
+  scope :econom_car, -> {where(:type_wagon => 'Плацкарт')}
+  scope :com_car, -> {where(:type_wagon => 'Купе')}
 
 end
