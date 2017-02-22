4f717fd156a458561cd0808d3d25c7a834aa18cd
AndryMac <promasalkin@yandex.ru>
2017-02-22 11:23:32 +0500

    Поправил миграцию у вагонов

--- db/migrate/20170214083933_create_carriages.rb
+++ db/migrate/20170214083933_create_carriages.rb
@@ -2,9 +2,13 @@ class CreateCarriages < ActiveRecord::Migration[5.0]
   def change
     create_table :carriages do |t|
 
+      t.integer :number
       t.string :type_wagon
       t.integer :number_top_place, default: 0
       t.integer :number_bottom_place, default: 0
+      t.integer :number_side_bottom_place, default: 0
+      t.integer :number_side_top_place, default: 0
+      t.integer :number_seat_place, default: 0
       t.belongs_to :train
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,16 @@
 ActiveRecord::Schema.define(version: 20170214083933) do
 
   create_table "carriages", force: :cascade do |t|
+    t.integer  "number"
     t.string   "type_wagon"
-    t.integer  "number_top_place",    default: 0
-    t.integer  "number_bottom_place", default: 0
+    t.integer  "number_top_place",         default: 0
+    t.integer  "number_bottom_place",      default: 0
+    t.integer  "number_side_bottom_place", default: 0
+    t.integer  "number_side_top_place",    default: 0
+    t.integer  "number_seat_place",        default: 0
     t.integer  "train_id"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.index ["train_id"], name: "index_carriages_on_train_id"
   end
 
