207c2862a608b7d5209ccb18eb06bbd1bead7501
AndryMac <promasalkin@yandex.ru>
2017-02-27 15:38:22 +0500

    валидация # вагона при создании, скоуп сортировки

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,24 +1,27 @@
 class Carriage < ApplicationRecord
-  validates :number,  :number_top_place, :number_bottom_place,  presence: true
+  validates :number_top_place, :number_bottom_place,  presence: true
+  validates :number, uniqueness: { scope: :train_id }
   belongs_to :train
 
-  before_validation :set_number
+  before_validation :set_number, on: :create
 
   TYPES = {
-      EconomCarriage: 'Плацкарт',
-      CoupeCarriage: 'Купе',
-      SedentaryCarriage: 'Сидячий',
-      SvCarriage: 'СВ'
+      econom: 'EconomCarriage',
+      coupe: 'CoupeCarriage',
+      sitting: 'SedentaryCarriage',
+      sv: 'SvCarriage'
   }
 
-  def self.types
-    TYPES
-    end
+  TYPES.each do |key, value|
+    scope key.to_sym, -> { where(type: value) }
+  end
 
-  scope :EconomCarriage, -> {where(type: 'Плацкарт')}
-  scope :CoupeCarriage, -> {where(type: 'Купе')}
-  scope :SedentaryCarriage, -> {where(type: 'Сидячий')}
-  scope :SvCarriage, -> {where(type: 'СВ')}
+  # scope :EconomCarriage, -> {where(type: 'Плацкарт')}
+  # scope :CoupeCarriage, -> {where(type: 'Купе')}
+  # scope :SedentaryCarriage, -> {where(type: 'Сидячий')}
+  # scope :SvCarriage, -> {where(type: 'СВ')}
+
+  scope :ordered, ->(order) { order(number: order ? 'asc' : 'desc') }
 
   private
 
