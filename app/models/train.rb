dc3b541449db37246bf03b96605de50592b3f4d4
AndryMac <promasalkin@yandex.ru>
2017-02-27 15:34:56 +0500

    Метод номер поезда и подсчет количества мест

--- app/models/train.rb
+++ app/models/train.rb
@@ -4,4 +4,17 @@ class Train < ApplicationRecord
   belongs_to :route
   has_many :tickets
   has_many :carriages
+
+  before_create :set_number
+
+  private
+
+  def set_number
+    self.number = Train.all.count + 1
+  end
+
+  def count_place(type, place)
+    carriages.where(type: type).sum(place)
+  end
+
 end
