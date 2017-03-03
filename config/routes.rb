e9404687ddc95e62bd512f4813a914a0f454d179
AndryMac <promasalkin@yandex.ru>
2017-03-04 00:30:04 +0500

    Исправил роут

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :trains
-  resources :railway_stations
+
   resources :routes
   resources :carriages
 
+  resources :railway_stations do
+   patch :update_position_order, on: :member
+  end
 
-  resources :update_position_order_railway_station
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
