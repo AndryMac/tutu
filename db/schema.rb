659bb11ef6973577745b1153a4e82f0d1a936861
AndryMac <promasalkin@yandex.ru>
2017-03-13 18:04:29 +0500

    Добавил время прибытия и отхода

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310064849) do
+ActiveRecord::Schema.define(version: 20170313124810) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "number",                   default: 0
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20170310064849) do
     t.integer "railway_station_id"
     t.integer "route_id"
     t.integer "position"
+    t.time    "arrival_time"
+    t.time    "departure_time"
   end
 
   create_table "routes", force: :cascade do |t|
