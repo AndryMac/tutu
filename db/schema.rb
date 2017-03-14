9942046849b9ad15d5ca2714064df7f440afd1ad
AndryMac <promasalkin@yandex.ru>
2017-03-14 18:24:38 +0500

    Имя и паспорт в миграцию

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313124810) do
+ActiveRecord::Schema.define(version: 20170314121240) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "number",                   default: 0
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20170313124810) do
     t.integer  "user_id"
     t.integer  "start_station_id"
     t.integer  "end_station_id"
+    t.string   "passenger_name"
+    t.string   "passenger_number"
     t.index ["end_station_id"], name: "index_tickets_on_end_station_id"
     t.index ["start_station_id"], name: "index_tickets_on_start_station_id"
     t.index ["train_id"], name: "index_tickets_on_train_id"
