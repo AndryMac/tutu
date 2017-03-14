65f222f13d4abf9b8114b7e686df9d4e69b4e5c1
AndryMac <promasalkin@yandex.ru>
2017-03-14 18:27:27 +0500

    Закомментировал связь с юзером

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   belongs_to :train
   belongs_to :start_station, class_name: "RailwayStation", foreign_key: :start_station_id
   belongs_to :end_station, class_name: "RailwayStation", foreign_key: :end_station_id
