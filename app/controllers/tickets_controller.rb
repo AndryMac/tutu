5f8aee946358e8c358f2169156a0a82b6480a389
AndryMac <promasalkin@yandex.ru>
2017-03-14 18:28:30 +0500

    Контроллер билетов

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,30 @@
+class TicketsController < ApplicationController
+   before_action :set_ticket, only: :show
+
+   def new
+     @ticket = Ticket.new(train_id: params[:train_id], start_station_id: params[:start_station_id], end_station_id: params[:end_station_id])
+   end
+
+   def show
+   end
+
+   def create
+     @ticket = Ticket.new(ticket_params)
+
+       if @ticket.save
+          redirect_to @ticket
+       else
+          render :new
+       end
+   end
+
+   private
+
+   def ticket_params
+     params.require(:ticket).permit(:train_id, :start_station_id, :end_station_id, :passenger_name, :passenger_number)
+   end
+
+   def set_ticket
+     @ticket = Ticket.find(params[:id])
+   end
+end
