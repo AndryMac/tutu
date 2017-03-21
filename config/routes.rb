64848be48ec6f8c449e6dccd058438e43b69d181
AndryMac <promasalkin@yandex.ru>
2017-03-21 12:19:27 +0500

    Неймспейс

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :trains do
-    resources :carriages, shallow: true
-  end
-  resources :routes
   resources :tickets
-
   resource :search, only: [:show, :create]
 
+  namespace :admin do
+    resources :railway_stations do
+      patch :update_position_order, on: :member
+      patch :update_time, on: :member
+    end
 
-  resources :railway_stations do
-   patch :update_position_order, on: :member
-   patch :update_time, on: :member
+    resources :trains do
+      resources :carriages, shallow: true
+    end
+
+    resources :routes
+    resources :tickets
   end
 
+  root 'searches#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
