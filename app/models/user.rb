cdb154fa0175df8f3939b34417502664df4204c4
AndryMac <promasalkin@yandex.ru>
2017-03-21 18:12:55 +0500

    Валидация имени и фамилии

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :tickets
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
