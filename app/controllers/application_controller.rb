32e154f855d2dbbb95b06b783e3ddbbfe3e556e5
AndryMac <promasalkin@yandex.ru>
2017-03-21 18:12:32 +0500

    метод перенаправления

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+
+  def after_sign_in_path_for(user)
+       if current_user.admin?
+         start_index_path
+       else
+         search_path
+       end
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys:[:first_name, :last_name]
+  end
 end
